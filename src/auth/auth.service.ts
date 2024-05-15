fd982f4fbc99133204e6d606f0ad7a86b14d20d9
sajjat43 <mdsajjat43@gmail.com>
2024-05-15 13:55:27 +0100

    form data show on console

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 // import {User, Bookmark} from '@prisma/client';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {
+
+  }
   signup() {
     return {msg: 'signup', status: 'success'};
   }
