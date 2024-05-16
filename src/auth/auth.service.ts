4ff1c6059821e743c77c604475e39d76c3ec5458
sajjat43 <mdsajjat43@gmail.com>
2024-05-16 10:55:09 +0100

    deplicate entry validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 // import {User, Bookmark} from '@prisma/client';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
-    
     // generate the password hash
     const hash = await argon.hash(dto.password);
 
     // save the user in db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-
-    // return the saved user
-    return user;
-
-    return { msg: 'signup', status: 'success' };
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      // Handle unique constraint violation error
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email address is already in use.');
+        }
+        throw error;
+      }
+      // Rethrow any other errors
+      throw error;
+    }
   }
+
   signin() {
     return { msg: 'signin', status: 'success' };
   }
