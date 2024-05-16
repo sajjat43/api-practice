9248c8ec0be65b431bcc2ad3f7c5031c5a4ef46b
sajjat43 <mdsajjat43@gmail.com>
2024-05-16 13:50:07 +0100

    login successfully

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,29 +6,14 @@ import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
-
-
+    
     @Post('signup')
-    // signup(@Req() req: Request) {
         signup(@Body() dto:AuthDto) {
-      
-       return this.authService.signup(dto);
+            return this.authService.signup(dto);
     }
-    // signup (
-    //     @Body('email') email: string,
-    //     @Body('password', ParseIntPipe) password: string,
-    // ){
-    //     console.log({
-    //         email,
-    //         type0fEmail: typeof email,
-    //         password,
-    //         type0fPassword: typeof password,
-    //     })
-    //     return this.authService.signup();
-    // }
-
+    
     @Post('signin')
-    signin() {
-        return this.authService.signin();
+    signin(@Body() dto:AuthDto) {
+        return this.authService.signin(dto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -36,7 +36,35 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'signin', status: 'success' };
+ async signin(dto: AuthDto) {
+
+    // find the user 
+    const user = 
+    await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    })
+    if (!user) 
+      throw new ForbiddenException(
+    'Credentials incorrect.'
+    );
+
+    // compare password 
+    const pwMatches = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    if(!pwMatches) 
+      throw new ForbiddenException(
+      'Credentials incorrect.'
+    );
+    console.log(user.email);
+    // send back the user 
+    delete user.hash;
+
+    return user;
+
+    // return { msg: 'signin', status: 'success' };
   }
 }
