d18d8f613229e80530ab671c67ad1999b9d1318a
sajjat43 <mdsajjat43@gmail.com>
2024-05-17 11:16:04 +0100

    jwt install and apply

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,10 +4,17 @@ import { AuthDto } from './dto';
 // import {User, Bookmark} from '@prisma/client';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+     private jwt: JwtService,
+    private config: ConfigService,
+    ) {} 
+
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
@@ -63,8 +70,20 @@ export class AuthService {
     // send back the user 
     delete user.hash;
 
-    return user;
+    // return user;
+    return this.signToken(user.id, user.email);
 
     // return { msg: 'signin', status: 'success' };
+  }
+  async signToken(userId: number, email: string): Promise<string>{
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret =this.config.get('JWT_SECRET')
+    return this.jwt.signAsync(payload,{
+      expiresIn: '15m',
+      secret: secret,
+    });
   }
 }
