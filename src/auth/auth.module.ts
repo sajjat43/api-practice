5b92ec1b8a7829a6590d726bd440a1294f8fbb6e
sajjat43 <mdsajjat43@gmail.com>
2024-06-04 13:59:29 +0100

    jwt error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 // import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [JwtModule.register({ })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,JwtStrategy],
 })
 export class AuthModule {}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy,'jwt',){
+    constructor(config: ConfigService){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secret0rKey: config.get('JWT_SECRET'),
+        });
+    }
+}
